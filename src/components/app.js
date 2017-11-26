64be1bf77dd4e0188b84ae1ad1568f6592a25a5b
Elnar Ibragimov <djelnar@gmail.com>
2017-11-26 03:29:28 +0300

    baberc

--- src/components/app.js
+++ src/components/app.js
@@ -1,4 +1,4 @@
 import React from 'react'
 
 export const App = props =>
-  <div>хуй</div>
+  <div>wellcum</div>
--- webpack.base.js
+++ webpack.base.js
@@ -8,10 +8,7 @@ const baseConfig = {
       test: /\.jsx?$/,
       exclude: /node_modules/,
       use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env', '@babel/preset-react']
-        }
+        loader: 'babel-loader'
       }
     }]
   },
